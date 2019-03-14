141d3e56607f3ecd672bf9986e1887b8c4674548
Erik Turøy Midtun <etm.midtun@gmail.com>
2019-03-14 13:52:53 +0100

    updated

--- zumo-robot.ino/zumo-robot.ino.ino
+++ zumo-robot.ino/zumo-robot.ino.ino
@@ -1,31 +1,82 @@
 /*
-Drive randomly by changing motor speed
+Drive forward and turn left or right when border is detected
+  - Only reflectionsensor 0 and 5 are used.
 */
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
-
-
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+ 
+#define LED 13
+ 
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1800 // 
+  
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        200
+#define FORWARD_SPEED     400
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+ 
 ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+boolean done = false;
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ 
+ZumoReflectanceSensorArray sensors;
+int numbTurns = 4;
 
-// This is the maximum speed the motors will be allowed to turn.
-// (400 lets the motors go at top speed; decrease to impose a speed limit)
-const int MAX_SPEED = 400;
-int leftMotorSpeed = 100;
-int rightMotorSpeed = 100;
-
+ 
 void setup()
 {
-  randomSeed(analogRead(A0));  // Set seed for random psudo-number generation
-  // Wait for the user button to be pressed and released
-  button.waitForButton();
+   sensors.init();
+   button.waitForButton();
+}
+
+boolean borderDetected(){
+  if (sensor_values[0]  > QTR_THRESHOLD || sensor_values[1]  > QTR_THRESHOLD || sensor_values[2]  > QTR_THRESHOLD || sensor_values[3]  > QTR_THRESHOLD || sensor_values[4]  > QTR_THRESHOLD
+  ||sensor_values[5]  > QTR_THRESHOLD)
+  {
+    return true;
+  }
+  return false;
+}
+
+void turn(int degrees){
+  int x90 = (240);
+  motors.setSpeeds(350,-350);
+  delay(x90);
+  motors.setSpeeds(0,0);
 }
 
 void loop()
 {
-   
-  motors.setSpeeds(leftMotorSpeed, rightMotorSpeed); //set left and right motor speed
-  delay(500);
-  leftMotorSpeed = random(MAX_SPEED);
-  rightMotorSpeed = random(MAX_SPEED);
+  if (numbTurns > 0) {
+    turn(180);
+    delay(50);
+    numbTurns --;
+  }
+  
+  
+  
+  /*
+  sensors.read(sensor_values);
+  
+if  (!done){
+  if (borderDetected())
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(200);
+    motors.setSpeeds(0, 0);
+    done = true;
+  }
+  else
+  {
+    // otherwise, go straight
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+}*/
 }
